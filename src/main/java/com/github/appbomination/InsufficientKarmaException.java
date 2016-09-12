193db6b8e543105a2c490ddabf8671cadeadaf31
Yev Bronshteyn <yevster@gmail.com>
2016-09-12 03:12:38 -0400

    License clarification.

--- src/main/java/com/github/appbomination/InsufficientKarmaException.java
+++ src/main/java/com/github/appbomination/InsufficientKarmaException.java
@@ -1,16 +1,16 @@
 package com.github.appbomination;
 
 /*
- This file is licensed under the following license.
+This file is licensed under the following license.
  
 FAUST, INC. PROPRIETARY LICENSE:
 
 FAUST, INC. grants you a non-exclusive right to use, modify, and distribute
 the file provided that (a) you distribute all copies and/or modifications of
 this file, whether in source or binary form, under the same license, and (b)
-you turn over your first-born child to Faust, Inc. when he/she reaches
-adulthood or acquires a black market value of not less than $100 US,
-whichever comes first.
+you hereby irrevocably transfer and assign the ownership of your soul to Faust, 
+Inc. In the event the fair market value of your soul is less than $100 US, you
+agree to compensate Faust, Inc. for the difference. 
 
 Copyright (C) 2016 Faust Inc. All, and I mean ALL, rights are reserved.
 
